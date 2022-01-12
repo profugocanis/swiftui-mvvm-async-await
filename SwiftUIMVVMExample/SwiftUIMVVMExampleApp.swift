2cd32ab98d49b01446edb209fa372f83df7acb02
olehkyrhan <profugo.canis@gmail.com>
2022-01-12 13:12:50 +0200

    Initial Commit

---
+++ SwiftUIMVVMExample/SwiftUIMVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMExampleApp.swift
+//  SwiftUIMVVMExample
+//
+//  Created by Oleh Kyrhan on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
