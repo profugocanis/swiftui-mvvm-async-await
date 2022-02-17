2bb1f8dceadaa33eee4152fcdc8e02361dcc9c22
olehkyrhan <profugo.canis@gmail.com>
2022-02-17 15:45:37 +0200

    added layoutDirection

--- SwiftUIMVVMExample/core/ui/BaseViewController.swift
+++ SwiftUIMVVMExample/core/ui/BaseViewController.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 
-class BaseViewController<Content> : UIHostingController<Content>, ViewModelSupportProtocol where Content : View {
+class BaseViewController<Content> : UIHostingController<AnyView>, ViewModelSupportProtocol where Content : View {
     
     internal var viewModels = [BaseViewModel?]()
+    
+    init(rootView: Content) {
+        let view = rootView.environment(\.layoutDirection, .leftToRight)
+        super.init(rootView: AnyView(view))
+    }
+    
+    @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     func addViewModel(_ vm: BaseViewModel?) {
         viewModels.append(vm)
